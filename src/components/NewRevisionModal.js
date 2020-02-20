0a956fd32728b17d0e625e08319c74e306415616
Matthew <matthew@stateofmind.agency>
2020-02-20 15:53:48 +0000

    Started to create a modal. Pure CSS for styling the backdrop.

---
+++ src/components/NewRevisionModal.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class NewRevisionModal extends Component {
+    render() {
+        return (
+            <div className="modal">
+                <div className="modal--element">
+                    
+                </div>
+            </div>
+        )
+    }
+}
