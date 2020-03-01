2d5f23a55d56a8e10101fd16950ab94a57936af1
Matthew <matthew@stateofmind.agency>
2020-03-01 12:47:18 +0000

    Added some basic styling for welcome message with projects.

--- src/components/dashboard/DashboardWelcome.js
+++ src/components/dashboard/DashboardWelcome.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 export default function DashboardWelcome({ username, projects }) {
+
     return (
         <div className="dashboard--welcome">
-            <h1>Welcome to your Project Management dashboard, {username}!</h1>
+            <h1 className="dashboard--welcome__title">Welcome to your Project Management dashboard, <br /> <span className="highlight">{username}!</span></h1>
             {projects.length > 0 
-            ? <p>Please select from one of your available projects</p>
+            ? <p className="dashboard--welcome__select">Please select from one of your available projects: {projects.map(project => <p className="dashboard--welcome__projects">{project.name}</p>)}</p>
             : <p>You dont have any active projects yet. Please get in touch with your Project Manager to receive an invite.</p>}
         </div>
     )
